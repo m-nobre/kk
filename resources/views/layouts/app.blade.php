0fc98f775715a36db940197a01760b663101086a
Mario Nobre <m.nobre@ymail.com>
2022-07-18 17:50:47 +0100

    vite build

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,18 +10,11 @@
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
-        {{-- <!-- Styles -->
+        <!-- Styles -->
         @livewireStyles
 
         <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
-
-            <!-- Styles -->
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-        @livewireStyles
-
-        <!-- Scripts -->
-        <script src="{{ asset('js/app.js') }}" defer></script>
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans antialiased">
         <x-jet-banner />
