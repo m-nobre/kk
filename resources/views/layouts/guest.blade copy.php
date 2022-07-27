847dbdf9ada42b1f5db60ea077133868d625eeba
Mario Nobre <m.nobre@ymail.com>
2022-07-27 13:54:04 +0100

    Added guest layout

--- resources/views/layouts/guest.blade copy.php
+++ resources/views/layouts/guest.blade copy.php
@@ -14,7 +14,6 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body>
-        @include('partials.nav')
         <div class="font-sans text-gray-900 antialiased">
             {{ $slot }}
         </div>
